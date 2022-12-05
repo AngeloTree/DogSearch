377650f89f9798cc3d99f91cd61d33be632601e0
Angelo Chaknis <angelo@Angelos-MacBook-Pro.local>
2022-12-05 09:51:15 -0700

    Reorganized files

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <button></button>
+    </div>
+  );
+};
+
+export default Home;
