4404ff382d3b9b052d325217e05b687dbf19d002
Angelo Chaknis <angelo@Angelos-MacBook-Pro.local>
2022-12-06 10:16:26 -0700

    Added button & input to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,12 @@ import "../styles/Home.css";
 const Home = () => {
   return (
     <div className="Home">
-      <button></button>
+      <div className="div-one">
+        <input type="text" />
+      </div>
+      <div className="div-two">
+        <button>test</button>
+      </div>
     </div>
   );
 };
