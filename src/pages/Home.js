4eb138b29176c9a8a59c782e4be10e89d5e1b4d1
Angelo Chaknis <angelo@Angelos-MacBook-Pro.local>
2022-12-06 22:44:33 -0700

    Added API connection using Fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,41 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "../styles/Home.css";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+
+    console.log("value is:", event.target.value);
+  };
+
+  const handleClick = (event) => {
+    console.log(search);
+  };
+
+  const [image, setImage] = useState("");
+
+  const fetchImage = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const imageData = await res.json();
+    setImage(imageData);
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
   return (
     <div className="Home">
       <div className="div-one">
-        <input type="text" />
+        <input id="message" type="text" onChange={handleChange} />
       </div>
       <div className="div-two">
-        <button>test</button>
+        <button onClick={handleClick}>test</button>
+      </div>
+      <div className="div-three">
+        <img src={image.message} alt="" />
       </div>
     </div>
   );
